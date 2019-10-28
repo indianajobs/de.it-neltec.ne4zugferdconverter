4f4f3fd32dc6a6a235df5495960de839487e02a5
Oliver Nelles <oliver.nelles@point-software.de>
2019-10-28 18:30:11 +0100

    Aufruf über Befehlszeile

--- NE4ZUGFeRD/App.xaml.cs
+++ NE4ZUGFeRD/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NE4ZUGFeRD.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,21 @@ namespace NE4ZUGFeRD
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            if (e.Args.Length == 2)
+            {
+                switch (e.Args[0])
+                {
+                    case "WRITE": break;
+                    case "READ": break;
+                    default: break;
+
+                }
+                Current.Shutdown();
+            }
+            wnd.Show();
+        }
     }
 }
